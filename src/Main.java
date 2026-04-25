f78b4af049e3c05b3af2b3ad6164998c125f526e
christiandev <christianakakpo13@gmailcom>
2026-04-25 01:18:26 +0000

    Codage du jeu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Main {
+    public static void main(String[] args) {
+        // Générer un nombre aléatoire entre 1 et 100
+        int secretNumber = 1 + (int)(Math.random() * 100);
+        int maxEssais = 10;
+        boolean gagne = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Jeu du nombre à deviner ===");
+        System.out.println("Trouvez le nombre entre 1 et 100.");
+        System.out.println("Vous avez 10 essais.");
+
+        for (int essai = 1; essai <= maxEssais; essai++) {
+            System.out.println("\nEssai " + essai + "/" + maxEssais + " : ");
+
+            int proposition = scanner.nextInt();
+
+            if (proposition == secretNumber) {
+                System.out.println("Bravo ! Vous avez trouvé en " + essai + " essai(s) !");
+                gagne = true;
+                break; // Sortir de la boucle
+            } else if (proposition < secretNumber) {
+                System.out.println("Trop petit ! Proposez plus grand.");
+            } else {
+                System.out.println("Trop grand ! Proposez plus petit.");
+            }
         }
+
+        // Si la boucle s'est terminée sans gagner
+        if (!gagne) {
+            System.out.println("\nPerdu ! Le nombre était : " + secretNumber);
+        }
+
+        scanner.close();
     }
 }
